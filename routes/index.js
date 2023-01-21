f469f2a401d6b3f2303bdbce95cad2ee68187e42
patriciacostadacruz <patriciacstcz@gmail.com>
2023-01-21 13:01:55 +0100

    Adds home link in navbar to view home page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Nakuru team' });
+  const user = req.session.currentUser;
+  res.render('index', { title: 'Nakuru team', user: user });
 });
 
 /* profile route */
